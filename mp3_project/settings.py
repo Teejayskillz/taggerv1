a10fee04e5438648feed2b86711be1ff297357d2
Teejayskillz <Teejayadewale87@gmail.com>
2025-07-12 10:47:26 +0100

    ADDED STATIC ROOT

--- mp3_project/settings.py
+++ mp3_project/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static']
-
+STATICE_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
